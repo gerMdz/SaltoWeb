15f069768b0b8c0bd0d119f69d35823026c70c3a
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2021-12-17 21:18:59 -0300

    Agregando MinishlinkWebPush

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Minishlink\Bundle\WebPushBundle\MinishlinkWebPushBundle::class => ['all' => true],
 ];
